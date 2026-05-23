f211cfb8fbbdb456d0478668cc9b13fa0a11a217
Sententia (Pankaekz) <135720862+SSententia@users.noreply.github.com>
2026-05-23 09:37:25 +0000

    Dependency fix 4

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -117,8 +117,8 @@ android {
         targetCompatibility = JavaVersion.VERSION_17
     }
 
-    compilerOptions {
-    jvmTarget.set(org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_17)
+    kotlinOptions {
+        jvmTarget = "17"
     }
 
     // see https://github.com/HeliBorg/HeliBoard/issues/477
